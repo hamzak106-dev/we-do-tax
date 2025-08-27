735a2b4b29f4fb477c41cf6c0f0bb37dd8a5279c
Faizan Ali <faizan.ali@bitandbytes.net>
2025-08-28 01:22:46 +0500

    api fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = { 
-  output: 'export',
   images: {
     unoptimized: true, // ✅ this disables image optimization that breaks static export
     domains: ["pixabay.com", "randomuser.me", "cdn.prod.website-files.com"],
